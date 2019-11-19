cd8b3a8f60aa74b41e708130f3b291fcba0ae67d
Adrian Forde <adrianforde@Adrians-MacBook-Pro.local>
2019-11-19 16:05:08 -0400

    Updates

---
+++ precache-manifest.343741cbc7fbc1288b3cfcf45524576c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d8ad4ecb3535eca6ab64a1fd1554cc",
+    "url": "/DawnOfHopee/weather-app/index.html"
+  },
+  {
+    "revision": "56291ed79744cde4a828",
+    "url": "/DawnOfHopee/weather-app/static/css/main.1586f561.chunk.css"
+  },
+  {
+    "revision": "ec14273d9e1f550b371b",
+    "url": "/DawnOfHopee/weather-app/static/js/2.89b88ace.chunk.js"
+  },
+  {
+    "revision": "56291ed79744cde4a828",
+    "url": "/DawnOfHopee/weather-app/static/js/main.ffcba22c.chunk.js"
+  },
+  {
+    "revision": "30f9c9bd29a8bbe08a41",
+    "url": "/DawnOfHopee/weather-app/static/js/runtime-main.5a328833.js"
+  },
+  {
+    "revision": "c5de5631eab0a09040af62753f3fd144",
+    "url": "/DawnOfHopee/weather-app/static/media/broken_cloud.c5de5631.svg"
+  },
+  {
+    "revision": "e972f64f1134ed711b2a26f48ea2f06d",
+    "url": "/DawnOfHopee/weather-app/static/media/cloud.e972f64f.svg"
+  },
+  {
+    "revision": "f36a0ff0e3b09676b56a3cc4a7463ce2",
+    "url": "/DawnOfHopee/weather-app/static/media/overcast.f36a0ff0.svg"
+  },
+  {
+    "revision": "4fa3d2b310b56d3aa1d63ef598f7430d",
+    "url": "/DawnOfHopee/weather-app/static/media/rain.4fa3d2b3.svg"
+  },
+  {
+    "revision": "cb7d74c543b4b260fa7bcd04aba2bcbf",
+    "url": "/DawnOfHopee/weather-app/static/media/snow.cb7d74c5.svg"
+  },
+  {
+    "revision": "30bad1ddf1a303448a95e543b1b0a9d3",
+    "url": "/DawnOfHopee/weather-app/static/media/storm.30bad1dd.svg"
+  },
+  {
+    "revision": "056175ba851dfc060060f2bbe5ed376f",
+    "url": "/DawnOfHopee/weather-app/static/media/sunny.056175ba.svg"
+  }
+]);
